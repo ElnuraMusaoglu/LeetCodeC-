9afd304710b3f986e07e6240ed516e3f13a0a45a
Elnura Musaoglu <elnuramusaoglu@gmail.com>
2022-06-01 23:59:34 +0300

    'Longestupdated'

--- Easy/14LongestCommonPrefix.cs
+++ Easy/14LongestCommonPrefix.cs
@@ -6,7 +6,7 @@ namespace LeetCode
     {
         public static void Main()
         {
-            string[] list = { "leetcode", "leetd", "leed","leeds" };
+            string[] list = { "leeetcode", "leeetd", "leeet","leeetds" };
             Console.WriteLine(LongestCommonPrefix(list).ToString());
         }
 
@@ -14,28 +14,36 @@ namespace LeetCode
         {
             if (strs == null || strs.Length == 0) return "";
             if (strs.Length == 1) return strs[0];
+
             int minLen = Int32.MaxValue;
+
             foreach (String str in strs)
                 minLen = Math.Min(minLen, str.Length);
+
             int low = 1;
             int high = minLen;
+
             while (low <= high)
             {
                 int middle = (low + high) / 2;
+
                 if (isCommonPrefix(strs, middle))
                     low = middle + 1;
                 else
                     high = middle - 1;
             }
+
             return strs[0].Substring(0, (low + high) / 2);
         }
 
         private static bool isCommonPrefix(String[] strs, int len)
         {
-            String str1 = strs[0].Substring(0, len);
+            String subString = strs[0].Substring(0, len);
+            
             for (int i = 1; i < strs.Length; i++)
                 if (!strs[i].StartsWith(str1))
                     return false;
+
             return true;
         }
     }
