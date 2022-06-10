2e2ad5a79e2fd6d8fa6dae56e6bf989d93b651f7
Elnura Arslan <elnuraarslan@gmail.com>
2022-06-10 17:56:10 +0300

    MergeTwoLists

---
+++ LeetCodeCSharp/Easy/21MergeTwoLists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeCSharp
+{
+    public partial class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if(list1==null) return list2;
+            if(list2==null) return list1;
+
+            ListNode result = new ListNode(-1000, null);
+            ListNode p = list1, q = list2, current = result;
+
+            while (p != null || q != null)
+            {
+                int val1 = p != null ? p.val : 1000;
+                int val2 = q != null ? q.val : 1000;
+
+                if (p != null && val1 <= val2)
+                {
+                    current.next = new ListNode(val1, null);
+                    current = current.next;
+                    p = p != null ? p.next : null;
+                }
+                if (q != null && val2 <= val1)
+                {
+                    current.next = new ListNode(val2, null);
+                    current = current.next;
+                    q = q != null ? q.next : null;
+                }
+            }
+
+            return result.next;
+        }
+    }
+}
--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -9,10 +9,12 @@ namespace LeetCodeCSharp
         public static void Main()
         {
             Solution solution = new Solution();
-            //ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, null)))));
-            ListNode l1 = new ListNode(1, new ListNode(2, null));
+            //ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+            //ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
+            ListNode l1 = new ListNode(2, null);
+            ListNode l2 = new ListNode(1, null);
 
-            var result = solution.RemoveNthFromEnd(l1, 2);
+            var result = solution.MergeTwoLists(l1, l2);
             
 
         }
