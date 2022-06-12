772df618710bcac21577630fc29ee3feff287892
Elnura Arslan <elnuraarslan@gmail.com>
2022-06-12 23:09:00 +0300

    Reverse List

---
+++ LeetCodeCSharp/Easy/206ReverseLinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeCSharp
+{
+    public partial class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode prev = null, next = null, current = head;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return current;
+        }
+    }
+}
--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -9,12 +9,12 @@ namespace LeetCodeCSharp
         public static void Main()
         {
             Solution solution = new Solution();
-            //ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
+            ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4, null)));
             //ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4, null)));
-            ListNode l1 = new ListNode(2, null);
+            //ListNode l1 = new ListNode(2, null);
             ListNode l2 = new ListNode(1, null);
 
-            var result = solution.MergeTwoLists(l1, l2);
+            var result = solution.ReverseList(l1);
             
 
         }
