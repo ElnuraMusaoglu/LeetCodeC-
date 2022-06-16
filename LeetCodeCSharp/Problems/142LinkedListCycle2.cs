f983b835d5f35360bf097fa0946e7a05f42b7f35
Elnura Arslan <elnuraarslan@gmail.com>
2022-06-16 17:05:20 +0300

    Problems updated.

--- LeetCodeCSharp/Problems/142LinkedListCycle2.cs
+++ LeetCodeCSharp/Problems/142LinkedListCycle2.cs
@@ -6,6 +6,6 @@ namespace LeetCodeCSharp
 {
     public partial class Solution
     {
-      
+       
     }
 }
---
+++ LeetCodeCSharp/Problems/347TopKFrequentElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace LeetCodeCSharp
+{
+    public partial class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums == null || nums.Length < 2) return nums;
+
+            Dictionary<int, int> numCount = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (numCount.ContainsKey(num)) numCount[num] = numCount[num] + 1;
+                else numCount.Add(num, 1);
+            }
+
+            int[] result = numCount.OrderByDescending(x => x.Value).Select(item => item.Key).Take(k).ToArray();
+
+            return result;
+        }
+    }
+}
