b6b5c26df841361a384f7505d0e146772734d807
Elnura Arslan <elnuraarslan@gmail.com>
2022-07-07 18:14:42 +0300

    Problems added.

--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -14,7 +14,7 @@ namespace LeetCodeCSharp
             //ListNode l1 = new ListNode(2, null);
             ListNode l2 = new ListNode(1, null);
 
-            var result = solution.FirstUniqChar("loveleetcode");
+            var result = solution.KSmallestPairs(new int[] { 1, 7, 11 }, new int[] { 2, 4, 6 }, 3);
             
 
         }
