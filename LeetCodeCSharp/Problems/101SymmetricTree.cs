a58a9586b1604ac0f782a279cbba24e00c0ffdd4
Elnura Arslan <elnuraarslan@gmail.com>
2022-07-20 00:58:01 +0300

    Problems added.

---
+++ LeetCodeCSharp/Problems/101SymmetricTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeCSharp
+{
+    public partial class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return true;
+
+            return IsSymmetricUtil(root.left, root.right);
+        }
+
+        private bool IsSymmetricUtil(TreeNode left, TreeNode right)
+        {
+
+            if (left == null && right == null) return true;
+            if (left != null && right != null && left.val == right.val)
+                return IsSymmetricUtil(left.left, right.right) && IsSymmetricUtil(left.right, right.left);
+
+            return false;
+        }
+    }
+}
---
+++ LeetCodeCSharp/Problems/515FindLargestValueinEachTreeRow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeCSharp
+{
+    public partial class Solution
+    {
+        public IList<int> LargestValues(TreeNode root)
+        {
+            List<int> result = new List<int>();
+            if (root == null) return result;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                int maxValue = Int32.MinValue;
+
+                for (int i = 0; i < size; i++)
+                {
+                    TreeNode current = queue.Dequeue();
+                    maxValue = current.val > maxValue ? current.val : maxValue;
+                    if (current.left != null) queue.Enqueue(current.left);
+                    if (current.right != null) queue.Enqueue(current.right);
+                }
+
+                result.Add(maxValue);
+            }
+
+            return result;
+        }
+    }
+}
